5ff6ee47e86c4f1a75f490067f9e734842e92556
knknWEB <knkn.web@gmail.com>
2022-12-05 12:12:58 +0100

    v0.2
    Logowanie z połączeniem z bazą danych, tymczasowo bez roli . Widok logowania, zmiana w układzie katalogów.

--- projekt_KacperNowak/projektKN_stop_ejb/ejbModule/stopdlaodlewni/dao/UserDAO.java
+++ projekt_KacperNowak/projektKN_stop_ejb/ejbModule/stopdlaodlewni/dao/UserDAO.java
@@ -1,23 +1,29 @@
 package stopdlaodlewni.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import stopdlaodlewni.entities.User;
 
 @Stateless
 public class UserDAO {
-	
-	@PersistenceContext
-	EntityManager em;
-	
-	
+	// simulate finding user in DB
+	@PersistenceContext//(unitName = UNIT_NAME)
+
+	protected EntityManager em;
+
 	public void create(User user) {
 		em.persist(user);
 	}
 
-	public User  merge(User user) {
+	public User merge(User user) {
 		return em.merge(user);
 	}
 
@@ -29,4 +35,57 @@ public class UserDAO {
 		return em.find(User.class, id);
 	}
 
+	public List<User> getFullList() {
+		List<User> list = null;
+
+		Query query = em.createQuery("select p from User p");
+
+		try {
+			list = query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+	public User getUserFromDatabase(String login, String pass) {
+		User u = null;
+
+		Query query = em.createQuery("select u FROM User u where u.login=:login");
+		query.setParameter("login", login);
+
+		try {
+			User user = (User) query.getSingleResult();
+
+			if (login.equals(user.getLogin()) && pass.equals(user.getPassword())) {
+				u = new User();
+				u.setLogin(login);
+				u.setPassword(pass);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return u;
+	}
+
+	// simulate retrieving roles of a User from DB
+	public List<String> getUserRolesFromDatabase(User user) {
+	
+		
+		
+		ArrayList<String> roles = new ArrayList<String>();
+
+		if (user.getLogin().equals("user1")) {
+			roles.add("user");
+		}
+		if (user.getLogin().equals("user2")) {
+			roles.add("manager");
+		}
+		if (user.getLogin().equals("admin")) {
+			roles.add("admin");
+		}
+		return roles;
+	}
 }
